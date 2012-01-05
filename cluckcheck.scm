11d46eb3f3b6d40a6bbdd470948824471afea3cd
Stefan Edwards <saedwards.ecc@gmail.com>
2012-01-05 00:21:04 -0500

    more scaffolding; basically, this is the layout of how I would see a "Cluckcheck" run working. Not exactly happy with it, but I only spent 15 minutes on it, so it is not overly attrocious considering

--- cluckcheck.scm
+++ cluckcheck.scm
@@ -29,8 +29,46 @@
         "generate a list with random data that matches what is defined in spec"
         #f)
 
+    (define (spec-pass? spec result)
+        "verify that a result is in line with its specification"
+        #f)
+
     (define (cluckcheck property spec #!optional (tests 100) (verbose #f))
-        #f) 
+        " given a property and a specification, test if this spec holds for this property"
+        (if (valid-spec? spec)
+            (let test-loop ((test-offset 0)
+                            (test-data (generate-spec-data spec)
+                            (error-count 0)
+                            (failure-count 0)
+                            (pass-count 0)))
+                (if (< test-offset tests)
+                    (guard (cnd (display (format "--- Error!\n~a\n" cnd)) 
+                            (test-loop
+                                (+ test-offset 1)
+                                (generate-spec-data spec) 
+                                (+ error-count 1)
+                                failure-count
+                                pass-count))
+                        (if (spec-pass? spec (apply property test-data))
+                            (test-loop
+                                (+ test-offset 1)
+                                (generate-spoec-data spec)
+                                error-count
+                                failure-count
+                                (+ pass count 1))
+                            (begin
+                                (when verbose
+                                    (display "*** Failure!\n"))
+                                (test-loop
+                                    (+ test-offset 1)
+                                    (generate-spec-data spec)
+                                    error-count
+                                    (+ failure-count 1)
+                                    passcount))))
+                    (display (format "+++ Passing tests: ~a\n*** Failing tests: ~a\n--- Errors: ~a\n"
+                                pass-count
+                                failure-count
+                                error-count))))))
 
 	(define (for-all property gen #!optional (tests 100) . generators)
 		(let* ((values (map (lambda (x) (map (lambda (f) (f)) (cons gen generators))) (iota tests)))
