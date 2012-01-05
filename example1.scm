cad8a62bd6adcbbf8a670ebf1a117628d935dcd4
Stefan Edwards <saedwards.ecc@gmail.com>
2012-01-05 00:27:20 -0500

    might as well clean up the examples too

--- example1.scm
+++ example1.scm
@@ -3,13 +3,13 @@
 (use cluckcheck)
 
 (define (gen-even)
-	(let ((n (gen-int)))
-		(if (not (= 0 (modulo n 2)))
-			(+ n 1)
-			n)))
+    (let ((n (gen-int)))
+        (if (not (= 0 (modulo n 2)))
+            (+ n 1)
+             n)))
 
 (define (reversible s)
-	(string=? s (list->string (reverse (reverse (string->list s))))))
+    (string=? s (list->string (reverse (reverse (string->list s))))))
 
 (for-all even? gen-int 1000)
 (for-all even? gen-even 1000)
