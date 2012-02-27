842a04b8214e243637d87abe67ee701f686f6e0a
Stefan Edwards <saedwards.ecc@gmail.com>
2012-02-27 10:37:28 -0500

    more generators, basic spec parsing, basic data generation

--- cluckcheck.scm
+++ cluckcheck.scm
@@ -6,6 +6,12 @@
     (define (gen-int)
         (random 256))
 
+    (define (gen-real)
+        (/ (random 10000) 10000.0))
+
+    (define (gen-rational)
+        (/ (gen-int) (gen-int)))
+
     (define (gen-bool)
         (= (random 2) 0))
 
@@ -21,14 +27,72 @@
     (define (gen-vector gen)
         (list->vector (gen-list gen)))
 
+    (define (scalar-spec-item? item)
+        (or (eq? item 'int)
+            (eq? item 'char)
+            (eq? item 'string)
+            (eq? item 'bool)
+            (eq? item 'unit)
+            (eq? item 'real)
+            (eq? item 'rational)
+            (eq? item 'natural)))
+
+    (define (compound-spec-item? item)
+        (or (eq? item 'list)
+            (eq? item 'vector)))
+
     (define (valid-spec? spec)
         "check if the specification given to cluck check is a valid one"
-        #f)
-
+        (cond
+            ((null? spec) #t)
+            ((scalar-spec-item? (car spec))
+                (valid-spec? (cddr spec))) ;; cop out; should really check that cadr eq? '->
+            ((compound-spec-item? (car spec))
+                (if (and (>= (length (cdr spec)) 1)
+                         (scalar-spec-item (cadr spec)))
+                    (valid-spec? (cdddr spec))
+                    #f))
+            (else #f)))
+               
+    (define (generate-scalar-spec-data spec)
+        " a simple swith & call the correct generator; it's meant to 
+          reuse code between generate-spec-data & the compound data
+          generators, gen-list, gen-vector"
+        (cond
+            ((eq? (car spec) 'int)
+                (gen-int))
+            ((eq? (car spec) 'real)
+                (gen-real))
+            ((eq? (car spec) 'rational)
+                (gen-rational))
+            ((eq? (car spec) 'natural)
+                (gen-natural))
+            ((eq? (car spec) 'char)
+                (gen-char))
+            ((eq? (car spec) 'string)
+                (gen-string)) 
+            ((eq? (car spec) 'unit)
+                (void))
+            ((eq? (car spec) 'bool)
+                (gen-bool))))
+            
+    ;; it might be easier for large/complex systems if specs were "compiled" into lambdas
+    ;; that could be simply run for each pass of cluckcheck. 
+    ;; also, need to cut the last item off, since we don't want to generate data for the 
+    ;; result here...
     (define (generate-spec-data spec)
         "generate a list with random data that matches what is defined in spec"
-        #f)
-
+        (cond
+            ((null? spec) '())
+            ((scalar-spec-item? (car spec))
+             (cons (generate-scalar-spec-data (car spec)) (generate-spec-data (cdr spec))))
+            ((eq? (car spec) 'list)
+             (cons (generate-list (lambda () (generate-scalar-spec-data (cadr spec))))
+                   (generate-spec-data (cddr spec))))
+            ((eq? (car spec) 'vector)
+             (cons (generate-vector (lambda () (generate-scalar-spec-data (cadr spec))))
+                   (generate-spec-data (cddr spec))))))
+             
     (define (spec-pass? spec result)
         "verify that a result is in line with its specification"
         #f)
@@ -52,7 +116,7 @@
                         (if (spec-pass? spec (apply property test-data))
                             (test-loop
                                 (+ test-offset 1)
-                                (generate-spoec-data spec)
+                                (generate-spec-data spec)
                                 error-count
                                 failure-count
                                 (+ pass count 1))
@@ -68,7 +132,8 @@
                     (display (format "+++ Passing tests: ~a\n*** Failing tests: ~a\n--- Errors: ~a\n"
                                 pass-count
                                 failure-count
-                                error-count))))))
+                                error-count))))
+            (display "Invalid specification\n")))
 
     (define (for-all property gen #!optional (tests 100) . generators)
         (let* ((values (map (lambda (x) (map (lambda (f) (f)) (cons gen generators))) (iota tests)))
